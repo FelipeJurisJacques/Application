3b7cc3638f2d18448be55ef51daac5e871c459c6
Felipe Juris Jacques <felipejuris2016@outlook.com>
2024-08-29 13:42:08 -0300

    console log de erros

--- Source/Core/Console.cs
+++ Source/Core/Console.cs
@@ -4,11 +4,28 @@ namespace Application.Source.Core
 {
     public class Console(IJSRuntime js)
     {
-        private readonly IJSRuntime _js = js;
+        private readonly IJSRuntime js = js;
 
         public void Log(string message)
         {
-            _js.InvokeVoidAsync("console.log", message);
+            js.InvokeVoidAsync("console.log", message);
+        }
+
+        public void Error(string error)
+        {
+            js.InvokeVoidAsync("console.error", error);
+        }
+
+        public void Error(Exception error)
+        {
+            if (error.StackTrace == null)
+            {
+                Error(error.Message);
+            }
+            else
+            {
+                Error(error.Message + error.StackTrace.ToString());
+            }
         }
     }
 }
